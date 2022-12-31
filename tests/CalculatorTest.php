b65fc5c7b04a18f11f6073fd6e1171d9c51fe7ff
heba adel <106149360+heba839@users.noreply.github.com>
2023-01-01 01:50:34 +0200

    Add files via upload
    task 1 unit testing

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require './src/Calculator.php';
+class CalculatorTest extends TestCase{
+    public function testAdd(){
+$c=new Calculator;
+$c=add
+
+    }
+}
+
+?>
